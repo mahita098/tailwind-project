dd3aa3077f893e95925cb1dff0887200269f4668
Mahita Ramgolam <mahita098@gmail.com>
2024-08-09 16:53:54 +0400

    react and tailwind set up

---
+++ tailwind-project/src/App.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+
+function App() {
+  return <div className="">content</div>;
+}
+
+export default App;
---
+++ tailwind-project/src/Header.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function Header() {
+  return (
+    <div className="h-20 bg-gray-100">
+      <ul className="flex gap-3">
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Header;
